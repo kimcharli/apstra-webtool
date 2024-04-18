abaf98b9f686ff2c459e817bd30401a3d854884f
Charlie Kim <kimcharli@gmail.com>
2024-04-18 18:59:00 -0400

    started with nicegui

--- webtool/components/apstra_server.py
+++ webtool/components/apstra_server.py
@@ -1,129 +1,212 @@
-import json
-from pathlib import Path
-import asyncio
-import aiohttp
-from dataclasses import dataclass
-from typing import Any, Optional, Tuple
+# import json
+# from pathlib import Path
+# import asyncio
+# import aiohttp
+# from dataclasses import dataclass
+# from typing import Any, Optional, Tuple
 
-from reactpy import component, event, hooks, html
+# from reactpy import component, event, hooks, html
+
+# from ck_apstra_api.apstra_session import CkApstraSession
+
+
+# class ApstraServer:
+#     host = None
+#     port = None
+#     username = None
+#     password = None
+#     logging_level = 'DEBUG'
+#     apstra_server = None  # CkApstraSession
+
+#     @classmethod
+#     def login(cls, host, port, username, password) -> Tuple[Optional[CkApstraSession], Optional[Any]]:
+#         """
+#         Login to the ApstraServer and return version and the error message
+#         """
+#         cls.apstra_server = CkApstraSession(host, port, username, password)
+#         if cls.apstra_server.last_error:
+#             return cls.apstra_server.version, cls.apstra_server.last_error
+#         cls.host = host
+#         cls.port = port
+#         cls.username = username
+#         cls.password = password
+#         return cls.apstra_server.version, "ok"    
+
+
+# @component
+# def ApstraServerComponent():
+#     apstra_host, set_apstra_host = hooks.use_state('10.85.192.50')
+#     apstra_port, set_apstra_port = hooks.use_state(443)
+#     apstra_user, set_apstra_user = hooks.use_state('admin')
+#     apstra_password, set_apstra_password = hooks.use_state('zaq1@WSXcde3$RFV')
+#     apstra_version, set_apstra_version = hooks.use_state('')
+#     apstra_status, set_apstra_status = hooks.use_state('')
+
+#     @event(prevent_default=True)
+#     async def handle_login(event):
+#         try:
+#             async with aiohttp.ClientSession('http://localhost:8000') as session:
+#                 async with session.post(
+#                     f"/login",
+#             # async with aiohttp.ClientSession() as session:
+#             #     async with session.post(
+#             #         f"http://localhost:8000/login",
+#                     json={"host": apstra_host, "port": apstra_port, "username": apstra_user, "password": apstra_password},
+#                 ) as response:
+#                     # assert await response.status == 200
+#                     response.raise_for_status()
+#                     data = await response.json()
+#                     set_apstra_version(data["version"])
+#                     set_apstra_status(data["status"])
+#         except Exception as e:
+#             set_apstra_status(str(e))
+
+#     return html.form(
+#         html.button({"type": "submit", "on_click": handle_login}, "Login"),
+#         html.table(
+#             {   "style": {"border": "1px solid black"}, 
+#                 "class_name": "border1"
+#             },
+#             html.tr(
+#                 html.th("Version"),
+#                 html.th("Status"),
+#                 html.th("Host"),
+#                 html.th("Port"),
+#                 html.th("User"),
+#                 html.th("Password"),
+#             ),
+#             html.tr(
+#                 html.td(
+#                     html.label(apstra_version)
+#                 ),
+#                 html.td(
+#                     html.label(apstra_status)
+#                 ),
+#                 html.td(
+#                     html.input(
+#                         {
+#                             "type": "text",
+#                             "placeholder": "Apstra Host",
+#                             "value": apstra_host,
+#                             "on_change": lambda event: set_apstra_host(event["target"]["value"]),
+#                         }
+#                     ),
+#                 ),
+#                 html.td(
+#                     html.input(
+#                         {
+#                             "type": "number",
+#                             "placeholder": "Apstra Port",
+#                             "value": apstra_port,
+#                             "on_change": lambda event: set_apstra_port(event["target"]["value"]),
+#                         }
+#                     ),
+#                 ),
+#                 html.td(
+#                     html.input(
+#                         {
+#                             "type": "text",
+#                             "placeholder": "Apstra User",
+#                             "value": apstra_user,
+#                             "on_change": lambda event: set_apstra_user(event["target"]["value"]),
+#                         }
+#                     ),
+#                 ),
+#                 html.td(
+#                     html.input(
+#                         {
+#                             "type": "password",
+#                             "placeholder": "Apstra Password",
+#                             "value": apstra_password,
+#                             "on_change": lambda event: set_apstra_password(event["target"]["value"]),
+#                         }
+#                     ),
+#                 ),
+#             )
+#         )
+#     )
+
+from typing import Any, Optional, Tuple
+from dataclasses import dataclass
+import httpx
+from contextlib import contextmanager
+from nicegui import ui
+import logging
 
 from ck_apstra_api.apstra_session import CkApstraSession
 
-
+@dataclass
 class ApstraServer:
-    host = None
-    port = None
-    username = None
-    password = None
+    host: str = '10.85.192.50'
+    port: str = '443'
+    username: str = 'admin'
+    password: str =  'zaq1@WSXcde3$RFV'
     logging_level = 'DEBUG'
-    apstra_server = None  # CkApstraSession
+    # apstra_server = None  # CkApstraSession
+    version: str = None
+    status: str = None
 
-    @classmethod
-    def login(cls, host, port, username, password) -> Tuple[Optional[CkApstraSession], Optional[Any]]:
+    # @classmethod
+    def login(self, host, port, username, password) -> Tuple[Optional[CkApstraSession], Optional[Any]]:
         """
         Login to the ApstraServer and return version and the error message
         """
-        cls.apstra_server = CkApstraSession(host, port, username, password)
-        if cls.apstra_server.last_error:
-            return cls.apstra_server.version, cls.apstra_server.last_error
-        cls.host = host
-        cls.port = port
-        cls.username = username
-        cls.password = password
-        return cls.apstra_server.version, "ok"    
+        self.apstra_server = CkApstraSession(host, port, username, password)
+        self.version = self.apstra_server.version
+        self.status = self.apstra_server.last_error or "ok"
+        if self.apstra_server.last_error:
+            return self.apstra_server.version, self.apstra_server.last_error
+        self.host = host
+        self.port = port
+        self.username = username
+        self.password = password
+        logging.warning(f"ApstraServer::login {ApstraServer=}")
+        return self.apstra_server.version, "ok"    
+
+# global variable
+apstra_server = ApstraServer()
+
+@contextmanager
+def disable(button: ui.button):
+    button.disable()
+    try:
+        yield
+    finally:
+        button.enable()
+
+async def login(button: ui.button) -> None:
+    with disable(button):
+        async with httpx.AsyncClient() as client:
+            response = await client.post('http://localhost:8000/login', json={
+                'host': apstra_server.host,
+                'port': apstra_server.port,
+                'username': apstra_server.username,
+                'password': apstra_server.password,
+            })
+            ui.notify(f'Response code: {response.status_code}')
 
 
-@component
-def ApstraServerComponent():
-    apstra_host, set_apstra_host = hooks.use_state('10.85.192.50')
-    apstra_port, set_apstra_port = hooks.use_state(443)
-    apstra_user, set_apstra_user = hooks.use_state('admin')
-    apstra_password, set_apstra_password = hooks.use_state('zaq1@WSXcde3$RFV')
-    apstra_version, set_apstra_version = hooks.use_state('')
-    apstra_status, set_apstra_status = hooks.use_state('')
-
-    @event(prevent_default=True)
-    async def handle_login(event):
-        try:
-            async with aiohttp.ClientSession('http://localhost:8000') as session:
-                async with session.post(
-                    f"/login",
-            # async with aiohttp.ClientSession() as session:
-            #     async with session.post(
-            #         f"http://localhost:8000/login",
-                    json={"host": apstra_host, "port": apstra_port, "username": apstra_user, "password": apstra_password},
-                ) as response:
-                    # assert await response.status == 200
-                    response.raise_for_status()
-                    data = await response.json()
-                    set_apstra_version(data["version"])
-                    set_apstra_status(data["status"])
-        except Exception as e:
-            set_apstra_status(str(e))
-
-    return html.form(
-        html.button({"type": "submit", "on_click": handle_login}, "Login"),
-        html.table(
-            {   "style": {"border": "1px solid black"}, 
-                "class_name": "border1"
-            },
-            html.tr(
-                html.th("Version"),
-                html.th("Status"),
-                html.th("Host"),
-                html.th("Port"),
-                html.th("User"),
-                html.th("Password"),
-            ),
-            html.tr(
-                html.td(
-                    html.label(apstra_version)
-                ),
-                html.td(
-                    html.label(apstra_status)
-                ),
-                html.td(
-                    html.input(
-                        {
-                            "type": "text",
-                            "placeholder": "Apstra Host",
-                            "value": apstra_host,
-                            "on_change": lambda event: set_apstra_host(event["target"]["value"]),
-                        }
-                    ),
-                ),
-                html.td(
-                    html.input(
-                        {
-                            "type": "number",
-                            "placeholder": "Apstra Port",
-                            "value": apstra_port,
-                            "on_change": lambda event: set_apstra_port(event["target"]["value"]),
-                        }
-                    ),
-                ),
-                html.td(
-                    html.input(
-                        {
-                            "type": "text",
-                            "placeholder": "Apstra User",
-                            "value": apstra_user,
-                            "on_change": lambda event: set_apstra_user(event["target"]["value"]),
-                        }
-                    ),
-                ),
-                html.td(
-                    html.input(
-                        {
-                            "type": "password",
-                            "placeholder": "Apstra Password",
-                            "value": apstra_password,
-                            "on_change": lambda event: set_apstra_password(event["target"]["value"]),
-                        }
-                    ),
-                ),
-            )
-        )
-    )
 
 
+
+def content() -> None:
+    global apstra_server
+    with ui.grid(columns=6).classes('w-full gap-0'):
+        ui.label('Version').classes('font-bold border p-1')
+        ui.label('Status').classes('font-bold border p-1')
+        ui.label('Host').classes('font-bold border p-1')
+        ui.label('Port').classes('font-bold border p-1')
+        ui.label('Username').classes('font-bold border p-1')
+        ui.label('Password').classes('font-bold border p-1')
+
+        ui.input(apstra_server.version).classes('border p-1').bind_value(apstra_server, 'version')
+        ui.input(apstra_server.status).classes('border p-1').bind_value(apstra_server, 'status')
+        ui.input(value='10.85.192.50').classes('border p-1').bind_value(apstra_server, 'host')
+        ui.input(value='443').classes('border p-1').bind_value(apstra_server, 'port')
+        ui.input(value='admin').classes('border p-1').bind_value(apstra_server, 'username')
+        ui.input(password=True, password_toggle_button=True, value='zaq1@WSXcde3$RFV').classes('border p-1').bind_value(apstra_server, 'password')
+
+        with ui.button(on_click=lambda e: login(e.sender)):
+            ui.label('Login')
+            ui.image('/public/login.svg').classes('rounded-full w-8 h-8 ml-4')
+            # ui.button('Login', on_click=lambda: ui.notify('Login clicked')).classes('col-span-6 text-white bg-blue-500 border p-1')
--- webtool/main.py
+++ webtool/main.py
@@ -1,45 +1,204 @@
+# from fastapi.staticfiles import StaticFiles
+# from reactpy import component, html
+# from reactpy.backend.fastapi import configure, Options
+# from fastapi import Body, FastAPI, Request, UploadFile, Form, WebSocket
+# from starlette.websockets import WebSocketDisconnect
+# from fastapi.responses import HTMLResponse
+# import uvicorn
+# import logging
+# import os
+# from typing import Annotated
+# from pydantic import BaseModel
+
+# from webtool.components.apstra_server import ApstraServerComponent, ApstraServer
+# from webtool.components.generic_system import GenericSystemUploadComponent
+
+
+# The_Title = os.environ.get('WEB_TITLE', "Apstra Webtool")
+# # The_Title = "Apstra Webtool"
+# options = Options
+# the_title = [x for x in options.head if x['tagName'] == 'title']
+# the_title[0]['children'] = [The_Title]
+
+# # logging.error(f"after {options.head=}")
+
+# app = FastAPI()
+# app.mount("/public", StaticFiles(directory="webtool/public"), name="public")
+
+# config = uvicorn.Config(app, host='localhost', port=8001, log_level='debug', reload=True)
+
+
+
+# # @app.middleware("http")
+# # async def disect_request(request: Request, call_next):
+# #     logging.warning(f"TRACING: request {request.url} {request.headers=}")
+# #     body = await request.body()
+# #     logging.warning(f"TRACING: request body {body=}")
+# #     response = await call_next(request)
+# #     logging.warning(f"TRACING: response {response}, {type(response)=}")
+# #     return response
+
+
+# class LoginData(BaseModel):
+#     host: str
+#     port: int
+#     username: str
+#     password: str
+
+# @app.post("/login")
+# # async def login(host: Annotated[str, Form()], port: Annotated[str, Form()], username: Annotated[str, Form()], password: Annotated[str, Form()]):
+# async def login(login_data: LoginData):
+#     """
+#     login to the server
+#     """
+#     try:
+#         logging.warning(f"login {login_data=}")
+#         # async for chunk in request.stream():
+#         #     print(chunk)
+#         # logging.warning(f"login {host=} {port=} {username=} {password=}")
+#         version, status = ApstraServer.login(login_data.host, login_data.port, login_data.username, login_data.password)
+#         logging.warning(f"login {status=} {version=}")
+#         return {"status": status, "version": version}
+#     except Exception as e:
+#         logging.error(f"login {e=}")
+#         return {"status": str(e), "version": ""}
+
+
+# @app.post("/upload-xlsx")
+# async def upload_xlsx(request: Request, file: UploadFile):
+#     """
+#     take environment yaml file to init global_store
+#     """
+#     logging.warning(f"upload_xlsx {file.filename=}")
+#     file_content = await file.read()
+#     return {"status": "ok", "version": "1.0"}
+
+
+
+
+# @app.websocket("/ws")
+# async def websocket_endpoint(websocket: WebSocket):
+#     await websocket.accept()
+#     try:
+#         while True:
+#             data = await websocket.receive_text()
+#             await websocket.send_text(f"Message text was: {data}")
+#     except WebSocketDisconnect as e:
+#         logging.warning(f"websocket closed, {e=}")
+#         return
+
+
+# css_1 = html.link(
+#         {
+#         "rel": "stylesheet",
+#         "href": "/public/style.css"
+#         }
+#     )
+
+
+# html = """
+# <!DOCTYPE html>
+# <html>
+#     <head>
+#         <title>Chat</title>
+#     </head>
+#     <body>
+#         <h1>WebSocket Chat</h1>
+#         <form action="" onsubmit="sendMessage(event)">
+#             <input type="text" id="messageText" autocomplete="off"/>
+#             <button>Send</button>
+#         </form>
+#         <ul id='messages'>
+#         </ul>
+#         <script>
+#             var ws = new WebSocket("ws://localhost:8000/ws");
+#             ws.onmessage = function(event) {
+#                 var messages = document.getElementById('messages')
+#                 var message = document.createElement('li')
+#                 var content = document.createTextNode(event.data)
+#                 message.appendChild(content)
+#                 messages.appendChild(message)
+#             };
+#             function sendMessage(event) {
+#                 var input = document.getElementById("messageText")
+#                 ws.send(input.value)
+#                 input.value = ''
+#                 event.preventDefault()
+#             }
+#         </script>
+#     </body>
+# </html>
+# """
+
+# @app.get("/")
+# async def get():
+#     return HTMLResponse(html)
+
+# # @component
+# # def home():
+# #     return html.section(
+# #         css_1,
+# #         html.h1("Apstra Webtool"),
+# #         html.hr(),
+# #         ApstraServerComponent(),
+# #         html.hr(),
+# #         GenericSystemUploadComponent(),
+# #         html.hr(),
+# #         )
+
+# # configure(app, home, options)
+# # configure(app, home)
+
+
+
+# #!/usr/bin/env python3
+# from webtool.components import frontend
+# from fastapi import FastAPI
+
+# app = FastAPI()
+
+
+# @app.get('/')
+# def read_root():
+#     return {'Hello': 'NiceGUI!'}
+
+
+# frontend.init(app)
+
+# if __name__ == '__main__':
+#     print('Please start the app with the "uvicorn" command as shown in the start.sh script')
+
+
 from fastapi.staticfiles import StaticFiles
-from reactpy import component, html
-from reactpy.backend.fastapi import configure, Options
-from fastapi import Body, FastAPI, Request, UploadFile, Form, WebSocket
-from starlette.websockets import WebSocketDisconnect
-from fastapi.responses import HTMLResponse
-import uvicorn
+from nicegui import app, ui
+from dataclasses import dataclass
 import logging
-import os
-from typing import Annotated
-from pydantic import BaseModel
 
-from webtool.components.apstra_server import ApstraServerComponent, ApstraServer
-from webtool.components.generic_system import GenericSystemUploadComponent
+from webtool.components import all_pages
+from webtool.components import home_page
+from webtool.components import theme
 
+from webtool.components import apstra_server
 
-The_Title = os.environ.get('WEB_TITLE', "Apstra Webtool")
-# The_Title = "Apstra Webtool"
-options = Options
-the_title = [x for x in options.head if x['tagName'] == 'title']
-the_title[0]['children'] = [The_Title]
-
-# logging.error(f"after {options.head=}")
-
+from fastapi import FastAPI, Request
 app = FastAPI()
 app.mount("/public", StaticFiles(directory="webtool/public"), name="public")
 
-config = uvicorn.Config(app, host='localhost', port=8001, log_level='debug', reload=True)
 
 
 
-# @app.middleware("http")
-# async def disect_request(request: Request, call_next):
-#     logging.warning(f"TRACING: request {request.url} {request.headers=}")
-#     body = await request.body()
-#     logging.warning(f"TRACING: request body {body=}")
-#     response = await call_next(request)
-#     logging.warning(f"TRACING: response {response}, {type(response)=}")
-#     return response
+@app.middleware("http")
+async def disect_request(request: Request, call_next):
+    logging.warning(f"TRACING: request {request.url} {request.headers=}")
+    body = await request.body()
+    logging.warning(f"TRACING: request body {body=}")
+    response = await call_next(request)
+    logging.warning(f"TRACING: response {response}, {type(response)=}")
+    return response
 
 
-class LoginData(BaseModel):
+@dataclass
+class LoginData:
     host: str
     port: int
     username: str
@@ -56,7 +215,7 @@ async def login(login_data: LoginData):
         # async for chunk in request.stream():
         #     print(chunk)
         # logging.warning(f"login {host=} {port=} {username=} {password=}")
-        version, status = ApstraServer.login(login_data.host, login_data.port, login_data.username, login_data.password)
+        version, status = apstra_server.apstra_server.login(login_data.host, login_data.port, login_data.username, login_data.password)
         logging.warning(f"login {status=} {version=}")
         return {"status": status, "version": version}
     except Exception as e:
@@ -64,87 +223,20 @@ async def login(login_data: LoginData):
         return {"status": str(e), "version": ""}
 
 
-@app.post("/upload-xlsx")
-async def upload_xlsx(request: Request, file: UploadFile):
-    """
-    take environment yaml file to init global_store
-    """
-    logging.warning(f"upload_xlsx {file.filename=}")
-    file_content = await file.read()
-    return {"status": "ok", "version": "1.0"}
 
 
+# here we use our custom page decorator directly and just put the content creation into a separate function
+@ui.page('/')
+def index_page() -> None:
+    with theme.frame('Homepage'):
+        home_page.content()
 
 
-@app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
-    try:
-        while True:
-            data = await websocket.receive_text()
-            await websocket.send_text(f"Message text was: {data}")
-    except WebSocketDisconnect as e:
-        logging.warning(f"websocket closed, {e=}")
-        return
+# this call shows that you can also move the whole page creation into a separate file
+all_pages.create()
 
-
-css_1 = html.link(
-        {
-        "rel": "stylesheet",
-        "href": "/public/style.css"
-        }
-    )
-
-
-html = """
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Chat</title>
-    </head>
-    <body>
-        <h1>WebSocket Chat</h1>
-        <form action="" onsubmit="sendMessage(event)">
-            <input type="text" id="messageText" autocomplete="off"/>
-            <button>Send</button>
-        </form>
-        <ul id='messages'>
-        </ul>
-        <script>
-            var ws = new WebSocket("ws://localhost:8000/ws");
-            ws.onmessage = function(event) {
-                var messages = document.getElementById('messages')
-                var message = document.createElement('li')
-                var content = document.createTextNode(event.data)
-                message.appendChild(content)
-                messages.appendChild(message)
-            };
-            function sendMessage(event) {
-                var input = document.getElementById("messageText")
-                ws.send(input.value)
-                input.value = ''
-                event.preventDefault()
-            }
-        </script>
-    </body>
-</html>
-"""
-
-@app.get("/")
-async def get():
-    return HTMLResponse(html)
-
-# @component
-# def home():
-#     return html.section(
-#         css_1,
-#         html.h1("Apstra Webtool"),
-#         html.hr(),
-#         ApstraServerComponent(),
-#         html.hr(),
-#         GenericSystemUploadComponent(),
-#         html.hr(),
-#         )
-
-# configure(app, home, options)
-# configure(app, home)
+ui.run_with(
+    app,
+    mount_path='/',
+    title='Apstra Webtool',
+)
