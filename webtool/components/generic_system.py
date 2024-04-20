c67367b2635fd1416ccf284327ac7f589ea433c8
Charlie Kim <kimcharli@gmail.com>
2024-04-19 21:32:44 -0400

    implementing generic_system deploy

--- webtool/components/generic_system.py
+++ webtool/components/generic_system.py
@@ -5,17 +5,38 @@ import logging
 import pandas as pd
 from io import StringIO
 
+from ck_apstra_api import generic_system
+from webtool.components import apstra_server
+
 
 the_columns = [
     {"name": "blueprint", "label": "blueprint", "field": "blueprint"},
-    {"name": "label", "label": "label", "field": "label"},
-    {"name": "ae", "label": "ae", "field": "ae"},
-    {"name": "cts", "label": "cts", "field": "cts"},
+    {"name": "system_label", "label": "system_label", "field": "system_label"},
+    {"name": "is_external", "label": "is_external", "field": "is_external"},
+
     {"name": "speed", "label": "speed", "field": "speed"},
-    {"name": "tags", "label": "tags", "field": "tags"},
-    {"name": "server_intf", "label": "server_intf", "field": "server_intf"},
-    {"name": "switch", "label": "switch", "field": "switch"},
-    {"name": "switch_intf", "label": "switch_intf", "field": "switch_intf"},
+    {"name": "lag_mode", "label": "lag_mode", "field": "lag_mode"},
+    {"name": "ct_names", "label": "ct_names", "field": "ct_names"},
+    {"name": "gs_tags", "label": "gs_tags", "field": "gs_tags"},
+
+    {"name": "server_intf1", "label": "server_intf1", "field": "server_intf1"},
+    {"name": "switch1", "label": "switch1", "field": "switch1"},
+    {"name": "switch_intf1", "label": "switch_intf1", "field": "switch_intf1"},
+
+    {"name": "server_intf2", "label": "server_intf2", "field": "server_intf2"},
+    {"name": "switch2", "label": "switch2", "field": "switch2"},
+    {"name": "switch_intf2", "label": "switch_intf2", "field": "switch_intf2"},
+
+    {"name": "server_intf3", "label": "server_intf3", "field": "server_intf3"},
+    {"name": "switch3", "label": "switch3", "field": "switch3"},
+    {"name": "switch_intf3", "label": "switch_intf3", "field": "switch_intf3"},
+
+    {"name": "server_intf4", "label": "server_intf4", "field": "server_intf4"},
+    {"name": "switch4", "label": "switch4", "field": "switch4"},
+    {"name": "switch_intf4", "label": "switch_intf4", "field": "switch_intf4"},
+
+    {"name": "comment", "label": "comment", "field": "comment"},
+
 ]
 
 the_rows = []
@@ -39,7 +60,7 @@ def handle_csv_upload(e: events.UploadEventArguments) -> None:
 
     # logging.warning(f"handle_upload {the_columns=}")
     the_rows=[{col: row[col] for col in the_df.columns} for _, row in the_df.iterrows()]
-    the_ui_table = ui.table(columns=the_columns, rows=the_rows)
+    the_ui_table = ui.table(columns=the_columns, rows=the_rows).props("square outlined")
 
     # the_grouped = the_df.groupby('blueprint').apply(list).to_dict()
     the_grouped = {k: list(v) for k, v in the_df.groupby('blueprint')}
@@ -88,6 +109,18 @@ def clear_table() -> None:
     the_select_columns.delete()
 
 
+def deploy() -> None:
+    global the_df
+    all_gs_spec = {}
+    for bp_label in [x for x in the_df.groupby('blueprint').groups.keys()]:
+        bp_spec = all_gs_spec[bp_label] = {}
+        for gs_label in [x for x in the_df[the_df['blueprint'] == bp_label].groupby('system_label').groups.keys()]:
+            # bp_spec[gs_label] = [x for x in the_df[(the_df['blueprint'] == bp_label) & (the_df['system_label'] == gs_label)]]
+            bp_spec[gs_label] = the_df[(the_df['blueprint'] == bp_label) & (the_df['system_label'] == gs_label)].to_dict('records')
+    logging.warning(f"deploy {all_gs_spec=}")
+    generic_system.add_generic_system(apstra_server.apstra_server, all_gs_spec)
+    
+ 
 
 def content() -> None:
     global the_columns
@@ -96,4 +129,5 @@ def content() -> None:
         ui.upload(on_upload=handle_csv_upload, label="csv upload", auto_upload=True).props('accept=.csv')
         ui.button('Clear Table', on_click=clear_table)
         ui.button('Download Sample', on_click=lambda: ui.download('/public/sample_generic_system.csv'))
+        ui.button('Deploy', on_click=deploy)
 
